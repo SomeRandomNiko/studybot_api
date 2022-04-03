4b4282f2d9eeb4a23e0418addd7efb077e634cc4
Niko <perathonerniko@gmail.com>
2022-04-03 22:18:24 +0200

    Added digreg ids and mongodb to config

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,8 @@ dotenv.config();
 
 export default {
     port: process.env.PORT || 8080,
+    mongodbURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/studybot',
+    jwtSecret: process.env.JWT_SECRET || 'secret',
+    digregClientId: process.env.DIGREG_CLIENT_ID || '',
+    digregClientSecret: process.env.DIGREG_CLIENT_SECRET || '',
 }
